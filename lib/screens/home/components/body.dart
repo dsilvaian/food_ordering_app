8916e63651a64ddfe52da4daebd372ee8c1aed08
Jesdin Raphael <jesdinraphael@gmail.com>
2020-10-29 11:18:26 +0530

    create Discoun Card

--- lib/screens/home/components/body.dart
+++ lib/screens/home/components/body.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:food_ordering_app/components/search_box.dart';
-import 'category-list.dart';
-import 'item-list.dart';
+import 'package:food_ordering_app/screens/home/components/category-list.dart';
+import 'package:food_ordering_app/screens/home/components/item-list.dart';
+import 'package:food_ordering_app/screens/home/components/discount-card.dart';
 
 class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         SearchWidget(
           onChanged: (value) {},
         ),
         CategoryList(),
         ItemList(),
+        DiscountCard(),
       ],
     );
   }
