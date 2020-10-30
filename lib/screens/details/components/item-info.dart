4179ad63011e153f1c88fdca9da21b0a2607a826
Jesdin Raphael <jesdinraphael@gmail.com>
2020-10-30 12:14:19 +0530

    complete item details page ui

---
+++ lib/screens/details/components/item-info.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:food_ordering_app/constraints.dart';
+import 'package:food_ordering_app/screens/details/components/order-button.dart';
+import 'package:food_ordering_app/screens/details/components/title-price-rating.dart';
+
+class ItemInfo extends StatelessWidget {
+  const ItemInfo({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(20),
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+      ),
+      child: Column(
+        children: <Widget>[
+          shopName(name: "McDonald's"),
+          TitlePriceRating(
+            name: "Cheese Burger",
+            numberOfReviews: 24,
+            rating: 4,
+            price: 15,
+            onRatingChange: (value) {},
+          ),
+          Text(
+            // "But I must explain to you how all this mistaken idea of denouncing of a pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who.",
+            "But I must explain to you how all this mistaken idea of denouncing of a pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful. Nor again is there anyone who loves or pursues or desires to obtain pain of itself, because it is pain, but occasionally circumstances occur in which toil and pain can procure him some great pleasure.",
+            style: TextStyle(height: 1.3),
+          ),
+          OrderButton(press: () {}),
+        ],
+      ),
+    );
+  }
+
+  Row shopName({String name}) {
+    return Row(
+      children: <Widget>[
+        Icon(
+          Icons.location_on,
+          color: ksecondaryColor,
+        ),
+        SizedBox(width: 10),
+        Text(name),
+      ],
+    );
+  }
+}
--- lib/screens/details/details-screen.dart
+++ lib/screens/details/details-screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:food_ordering_app/constraints.dart';
 import 'package:food_ordering_app/screens/details/components/app-bar.dart';
 import 'package:food_ordering_app/screens/details/components/item-image.dart';
-import 'package:food_ordering_app/screens/details/components/title-price-rating.dart';
+import 'package:food_ordering_app/screens/details/components/item-info.dart';
 
 class DetailsScreen extends StatelessWidget {
   @override
@@ -27,52 +27,6 @@ class Body extends StatelessWidget {
         Expanded(
           child: ItemInfo(),
         ),
-      ],
-    );
-  }
-}
-
-class ItemInfo extends StatelessWidget {
-  const ItemInfo({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.all(20),
-      width: double.infinity,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          topLeft: Radius.circular(30),
-          topRight: Radius.circular(30),
-        ),
-      ),
-      child: Column(
-        children: <Widget>[
-          shopName(name: "McDonald's"),
-          TitlePriceRating(
-            name: "Cheese Burger",
-            numberOfReviews: 24,
-            rating: 4,
-            price: 15,
-            onRatingChange: (value) {},
-          ),
-        ],
-      ),
-    );
-  }
-
-  Row shopName({String name}) {
-    return Row(
-      children: <Widget>[
-        Icon(
-          Icons.location_on,
-          color: ksecondaryColor,
-        ),
-        SizedBox(width: 10),
-        Text(name),
       ],
     );
   }
