44abdfe2cd587c85b889a5251214fe94f9584ee0
dsilvaian <dsilvaian2000@gmail.com>
2020-11-27 03:09:25 +0530

    Display an image on  Cart Page when there is no item

--- lib/screens/cart/components/body.dart
+++ lib/screens/cart/components/body.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:food_ordering_app/constraints.dart';
 import 'package:food_ordering_app/screens/cart/cart_model.dart';
 import 'package:food_ordering_app/screens/cart/components/cart_product.dart';
@@ -18,34 +19,48 @@ class CartPageBody extends StatelessWidget {
 
     final cart = Provider.of<Cart>(context);
 
-    return SingleChildScrollView(
-      child: Container(
-        // width: MediaQuery.of(context).size.width,
-        // height: MediaQuery.of(context).size.height,
-        padding: EdgeInsets.all(15),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            ListView.builder(
-                physics: const NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: cart.items.length,
-                itemBuilder: (context, index) {
-                  String key = cart.items.keys.elementAt(index);
-                  CartItem _item = cart.items[key];
-                  return CartProduct(
-                      _item.id, key, _item.name, _item.quantity, _item.price);
-                }),
-            SizedBox(
-              height: 10,
-            ),
-            PromoCodeWidget(),
-            SizedBox(height: 10),
-            TotalCalculationWidget(),
-            CartCheckoutButton(press: () {})
-          ],
+    Widget child;
+
+    if (cart.items.isEmpty) {
+      child = Container(
+        child: Center(
+            child: FractionallySizedBox(
+          widthFactor: 0.7,
+          child: SvgPicture.asset(
+            "assets/icons/empty_cart.svg",
+          ),
+        )),
+      );
+    } else {
+      child = SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              ListView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: cart.items.length,
+                  itemBuilder: (context, index) {
+                    String key = cart.items.keys.elementAt(index);
+                    CartItem _item = cart.items[key];
+                    return CartProduct(
+                        _item.id, key, _item.name, _item.quantity, _item.price);
+                  }),
+              SizedBox(
+                height: 10,
+              ),
+              PromoCodeWidget(),
+              SizedBox(height: 10),
+              TotalCalculationWidget(),
+              CartCheckoutButton(press: () {})
+            ],
+          ),
         ),
-      ),
-    );
+      );
+    }
+
+    return child;
   }
 }
